81f8952142bfe1a6f530fa6162cf7ca875d0994a
Your Name <you@example.com>
2026-02-24 15:46:13 +0000

    hello world

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   description = "flake";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     st-src.url = "path:/home/ehsab/.config/builds/st";
   };
